0102d9e64f4f629d332795608c9c6cdcde6fca4e
pinja <pinja@Dastagiri>
2024-06-21 17:13:43 +0530

    Initial commit

---
+++ javaquiz/src/javaquiz/Questions.java
@@ -0,0 +1,59 @@
+package javaquiz;
+
+public class Questions {
+	private String Question;
+    private String option1;
+    private String option2;
+    private String option3;
+    private String option4;
+
+    public Questions(String question2, String option12, String option22, String option32, String option42) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getQuestion() {
+		return Question;
+	}
+
+	public void setQuestion(String question) {
+		Question = question;
+	}
+
+	public String getOption1() {
+		return option1;
+	}
+
+	public void setOption1(String option1) {
+		this.option1 = option1;
+	}
+
+	public String getOption2() {
+		return option2;
+	}
+
+	public void setOption2(String option2) {
+		this.option2 = option2;
+	}
+
+	public String getOption3() {
+		return option3;
+	}
+
+	public void setOption3(String option3) {
+		this.option3 = option3;
+	}
+
+	public String getOption4() {
+		return option4;
+	}
+
+	public void setOption4(String option4) {
+		this.option4 = option4;
+	}
+
+	public String getQuestion1() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
